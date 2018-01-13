6d676705ff9b58252d1c73b22abc2fba2f0de390
Ken McGarry <ken.mcgarry@sunderland.ac.uk>
2018-01-13 12:28:12 +0000

    Add files via upload

--- R/complex_networks_plots.R
+++ R/complex_networks_plots.R
@@ -20,6 +20,25 @@ bar_plot_drugtargets <- function(){
        labels = paste(names(tempnames)),cex=0.9)
 }
 
+# ggplot2 vewrsion of barplot - prettier?
+bar_plot_gg2 <- function(){
+  tempnames <- sort(table(drug_targets$TargetClass),decreasing = TRUE)
+  Frequency <- as.vector(tempnames)
+  pnames <- names(tempnames)
+  df <- data.frame(Frequency,pnames)
+  df$Frequency <- as.numeric(df$Frequency)
+
+  ggplot(df, aes(x = pnames,y=(Frequency))) + scale_x_discrete(limits = pnames) + 
+    geom_bar(stat="identity",fill="red")+
+    theme(axis.text.x=element_text(face="bold",angle=40,hjust=1,size=12)) +
+    theme(axis.text.y=element_text(face="bold",angle=0,hjust=1,size=12)) +
+    ylab("Frequency count of proteins") + 
+    xlab("")+
+    scale_y_continuous(expand = c(0,0),breaks = c(0,500,1000,1500,2000,2500,3000,3500,4000))+
+    theme(axis.title.y = element_text(color="black", size=14, face="bold"))
+   
+}
+
 
 # Do not plot the entire graph as its way too large - plot a small subset only
 plot_small <- function(){
@@ -52,21 +71,19 @@ plot_power <- function(){
   estimate_xmin(m)
   m$setXmin(105); m$setPars(2.644)
   
-  plot(m,xlab="Degree",ylab="CDF")
-  grid(lty = 6, col = "cornsilk2")
-  xaxp <- par("xaxp")
-  yaxp <- par("yaxp")
+  temp <- plot(m,xlab="Degree",ylab="CDF")
   
-  #abline(v=seq(xaxp[1], xaxp[2], (xaxp[2]-xaxp[1])/xaxp[3]), lty=6, col = "cornsilk2")
-  #abline(h=seq(yaxp[1], yaxp[2], (yaxp[2]-yaxp[1])/yaxp[3]), lty=6, col = "cornsilk2")
+  ggplot(data=temp,aes(x=x,y=y))+
+    geom_point()+
+    labs(title="",x="Degree distribution",y="CDF")
   
-  lines(m, col=2)
-  abline(h=c(1e-04,1e-03,1e-01,1e-00),v=c(1,2,5,10,20,50,100,200,500), col="gray", lty=3)
 }
 
 
 
+
 bar_plot_drugtargets()  # barplot of the protein types of drug targets
+bar_plot_gg2()  # Hadley Wickhams ggplot version
 plot_power()   # graph of degree power law
 
 

80f31186fc5685b44f91fcba2454af27e61297c7
Ken McGarry <ken.mcgarry@sunderland.ac.uk>
2018-01-26 14:52:04 +0000

    Add files via upload

--- R/complex_networks_tables.R
+++ R/complex_networks_tables.R
@@ -1,11 +1,17 @@
 # complex_networks_tables.R
 # plot the tables used in the paper.
 
-# Table for nodes sorted on betweenness measure.
+# Table for nodes sorted on betweenness etc measures.
 gs <- get_gstatistics(ppi_net)
 display_netstats(gs$net)
 
 
 print(xtable(head(gs$nodes[order(gs$nodes$betweenness,decreasing=TRUE),],15),
        display=c("s","g","f","f","g","g","f"), math.style.exponents = TRUE,digits=c(0,0,0,2,2,2,1)))
+
+print(xtable(head(gs$nodes[order(gs$nodes$betweenness,decreasing=FALSE),],15),
+             display=c("s","g","f","f","g","g","f"), math.style.exponents = TRUE,digits=c(0,0,0,2,2,2,1)))
+
+print(xtable(head(gs$nodes[order(gs$nodes$hubness,decreasing=TRUE),],15),
+             display=c("s","g","f","f","g","g","f"), math.style.exponents = TRUE,digits=c(0,0,0,2,2,2,1)))
 

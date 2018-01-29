c89bf3500a8293c21acc9c9f6f2de9530d595503
Ken McGarry <ken.mcgarry@sunderland.ac.uk>
2018-01-29 17:53:55 +0000

    Add files via upload

--- R/complex_networks_misc.R
+++ R/complex_networks_misc.R
@@ -2,7 +2,7 @@
 # part completed functions and code that has yet to be assigned.
 # Not called up as yet, as will cause errors.
 
-# unsure protein targets are part of the giant connected ppi network
+# unsure if protein targets are part of the giant connected ppi network
 # assign 1=target; 0=non-target to each protein
 
 un_targets <- (unique(drug_targets$Gene))        # 1,860 unique protein targets
@@ -13,6 +13,10 @@ length(un_ppi)
 joint_ppi <- un_targets[un_targets %in% un_ppi]  # 1,293 targets are part of giant connected network (we lose 567 targets!)
 not_ppi <- un_targets[!un_targets %in% un_ppi]  # here are the 567 targets)
 
+# remove multiple genes
+ppi$Gene_A <-gsub("|.*","",ppi$Gene_A)
+ppi$Gene_B <-gsub("\\|.*","",ppi$Gene_B)  # gsub("\\..*","",a)
+
 # dataframe containing targets and non-target proteins. Annotate with:
 # 1. target; 2. hub; 
 # create ppi network (igraph object) and annotate with target or not target
@@ -20,10 +24,15 @@ ppi_net <- graph.data.frame(ppi)
 ppi_net <- as.undirected(ppi_net); 
 ppi_net <- igraph::simplify(ppi_net)  # remove duplicates and self-loops
 ppi_net <- delete_isolates(ppi_net)
+delete.vertices(igraph::simplify(ppi_net), degree(ppi_net)==0)
 
 V(ppi_net)[1:vcount(ppi_net)]$target <- 0   # Intialise all to zeros
 V(ppi_net)[1:vcount(ppi_net)]$hub <- 0   # Intialise all to zeros
 V(ppi_net)[1:vcount(ppi_net)]$type <- "unknown"   # Intialise protein "type" to unknown
+
+# get main component only - ignore lessor weakly connected groups
+V(ppi_net)$comp <- components(ppi_net)$membership
+ppi_net <- induced_subgraph(ppi_net,V(ppi_net)$comp==1)
 
 ppi_net <- set_vertex_attr(ppi_net,"target",joint_ppi,1) # Now assign "1" if protein is a target (very neat!)
 gs <- get_gstatistics(ppi_net)

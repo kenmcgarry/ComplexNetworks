c9044c2b5151f9e8defc4a1356ff01fb2a99c972
Ken McGarry <ken.mcgarry@sunderland.ac.uk>
2018-02-08 15:31:15 +0000

    Add files via upload

--- R/complex_networks_classifiers.R
+++ R/complex_networks_classifiers.R
@@ -9,32 +9,32 @@ library("e1071")
 library(caret)
 
 #  matrix mmt [150 x 12997] contains the raw data, needs to split. Row 150 contains "class" labels.
-mmi <- t(mmt)
+mmi <- mmt[,1:6000]
 mmi <- data.frame(mmi)
-mmi$targets <- as.factor(mmi$targets)
 
-## Prepare a training and a test set ##
-ntrain <- round(n*0.8) # number of training examples
-tindex <- sample(n,ntrain) # indices of training samples
-xtrain <- x[tindex,]
-xtest <- x[-tindex,]
-ytrain <- y[tindex]
-
-#svm_model <- ksvm(targets~.,data=mmi,kernel="rbfdot",scaled=FALSE,kpar=list(sigma=0.05),C=1,cross=5)
-svm_model <- ksvm(targets~.,data=mmi,type="C-svc",kernel="vanilladot",C=100,cross=5,scaled=c()) #cross=5
-
-targettype <- predict(svm_model,mmi)
-table(targettype,mmi$targets)
-
-targettype <- as.numeric(targettype)  # change for prediction()
+mtest <- mmt[,6001:12000]
+mtest <- data.frame(mtest)
+#mmi$targets <- as.factor(mmi$targets)
 
 for (i in 1:length(targettype)){
   if(targettype[i] == 1) {  
-     targettype[i] <- 0}}
+     targettype[i] <- runif(1, 7.0, 9.9)}}
 for (i in 1:length(targettype)){
-  if(targettype[i] == 2) {  
-    targettype[i] <- 1}}
+  if(targettype[i] == 0) {  
+    targettype[i] <- runif(1,0.01,0.02)}}
 
+# http://amunategui.github.io/smote/  # Supersampling Rare Events - build up smaller target class
+# https://topepo.github.io/caret/index.html
+# http://www.rebeccabarter.com/blog/2017-11-17-caret_tutorial/
+# fit a random forest model (using ranger package), re-runs the model over 25 bootstrap samples and 
+# across 3 options of the tuning parameter
+targets <- (mmi[150,]); targets <- t(targets); rownames(targets) <- NULL; colnames(targets) <- NULL
+targets <- as.factor(targets)
+testtargets <- mtest[150,];testtargets <- t(testtargets);rownames(testtargets) <- NULL; colnames(testtargets) <- NULL 
+testtargets <- as.factor(testtargets)
+
+rf_fit <- train(as.factor(targets) ~., data = t(mmi),method = "ranger")
+targettype <- predict(rf_fit,t(mmi))
 
 # https://www.r-bloggers.com/a-small-introduction-to-the-rocr-package/
 pred <- prediction(targettype,mmi$targets)
@@ -45,12 +45,47 @@ plot(roc3.perf)
 roc4.perf <- performance(pred, "spec", "sens")
 plot(roc4.perf)
 
-# http://amunategui.github.io/smote/  # Supersampling Rare Events - build up smaller target class
-# https://topepo.github.io/caret/index.html
-# http://www.rebeccabarter.com/blog/2017-11-17-caret_tutorial/
-# fit a random forest model (using ranger package), re-runs the model over 25 bootstrap samples and 
-# across 3 options of the tuning parameter
-rf_fit <- train(as.factor(mmi$targets) ~ ., data = mmi,method = "ranger")
+# accuracy on training data
+pred <- prediction(as.numeric(targettype),targets)
+table(targettype,targets)
+roc1.perf <- performance(pred, "tpr", "fpr")
+plot(roc1.perf)
+
+# accuracy on test data
+targettype2 <- predict(rf_fit,t(mtest))
+pred2 <- prediction(as.numeric(targettype2),testtargets)
+table(targettype2,targets)
+roc2.perf <- performance(pred2, "tpr", "fpr")
+plot(roc2.perf)
+
+# poor accuracy might be solved by using fewer non-target exemplars
+mcrap <- t(mmt)
+mcrap <- as.data.frame(mcrap)
+positives <- mcrap[mcrap$targets == 1,]  # get all targets (1,443)
+negatives <- mcrap[mcrap$targets == 0,]  # get all targets (11,554)
+negatives <- sample_n(negatives, nrow(positives)) # only use 1,443 of them to match positives
+balanced_dat <- rbind(positives,negatives)
+
+## Prepare a training and a test set ##
+ntrain <- round(nrow(balanced_dat)*0.8) # number of training examples
+tindex <- sample(nrow(balanced_dat),ntrain) # indices of training samples
+xtrain <- data.frame(balanced_dat[tindex,])
+xtest <- data.frame(balanced_dat[-tindex,])
+
+ytrain <- xtrain[,150]  # class labels for training data
+ytest <- xtest[,150]   # class labels for test data
+ytest <- as.factor(ytest); #rownames(ytest) <- NULL; colnames(ytest) <- NULL
+#xtest <- t(xtest)
+xtest <- data.frame(xtest)
+
+# Ok, so retain Random Forest
+rf_fit <- train(as.factor(ytest) ~., data=(xtest), method = "ranger")
+#targettype <- predict(rf_fit,t(xtrain))
+
+
+
+
+
 
 
 

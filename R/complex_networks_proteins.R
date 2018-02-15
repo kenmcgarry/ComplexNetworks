60eb34dfaab843bc23a57861f977d11a1238b98c
Ken McGarry <ken.mcgarry@sunderland.ac.uk>
2018-02-15 15:48:57 +0000

    Add files via upload

--- R/complex_networks_proteins.R
+++ R/complex_networks_proteins.R
@@ -3,9 +3,9 @@
 # Now analyze main protein classes comprising candidate targets
 # Examine drug_targets dataframe
 
-GPCR <- filter(drug_targets,TargetClass == "GPCR")
-enzyme <- filter(drug_targets,TargetClass == "Enzyme")
-IC <- filter(drug_targets,TargetClass == "Ion channel")
-transporter <- filter(drug_targets,TargetClass == "Transporter")
+p_GPCR <- filter(drug_targets,TargetClass == "GPCR")
+p_enzyme <- filter(drug_targets,TargetClass == "Enzyme")
+p_IC <- filter(drug_targets,TargetClass == "Ion channel")
+p_transporter <- filter(drug_targets,TargetClass == "Transporter")
 
-
+# Alternatively, assess the drugs targetiing the k-core neighborhood.
